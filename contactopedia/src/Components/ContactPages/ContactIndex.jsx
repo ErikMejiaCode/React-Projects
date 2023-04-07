c7e64d159fddfcf2ff74e164ac3c10c29154df5f
Erik Mejia <erikdojo352@gmail.com>
2023-04-07 14:21:19 -0400

    add contact handler

--- contactopedia/src/Components/ContactPages/ContactIndex.jsx
+++ contactopedia/src/Components/ContactPages/ContactIndex.jsx
@@ -37,6 +37,19 @@ class ContactIndex extends React.Component {
     };
   }
 
+  handleAddContact = (newContact) => {
+    const newFinalContact = {
+      ...newContact,
+      id: this.state.contactList[this.state.contactList.length - 1].id + 1,
+      isFavorite: false,
+    };
+    this.setState((prevState) => {
+      return {
+        contactList: prevState.contactList.concat([newFinalContact]),
+      };
+    });
+  };
+
   render() {
     return (
       <div>
@@ -51,7 +64,7 @@ class ContactIndex extends React.Component {
             </div>
             <div className="row py-2">
               <div className="col-8 offset-2 row">
-                <AddContact />
+                <AddContact handleAddContact={this.handleAddContact} />
               </div>
             </div>
             <div className="row py-2">
