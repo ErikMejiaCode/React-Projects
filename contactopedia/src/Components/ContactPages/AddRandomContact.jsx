53c12967c5d15a6b79b3a13f698f76e687e775f4
Erik Mejia <erikdojo352@gmail.com>
2023-04-10 20:05:38 -0400

    random contact added

--- contactopedia/src/Components/ContactPages/AddRandomContact.jsx
+++ contactopedia/src/Components/ContactPages/AddRandomContact.jsx
@@ -1,16 +1,23 @@
 import { getRandomUser } from "../../Utilities/api";
 
-const GetRandomContact = async () => {
+const GetRandomContact = async (props) => {
   const responseFromApi = await getRandomUser();
   console.log(responseFromApi);
+
+  return props.handleAddRandomContact({
+    name:
+      responseFromApi.data.first_name + " " + responseFromApi.data.last_name,
+    email: responseFromApi.data.email,
+    phone: responseFromApi.data.phone_number,
+  });
 };
 
-const AddRandomContact = () => {
+const AddRandomContact = (props) => {
   return (
     <div>
       <button
         className="btn btn-success form-control"
-        onClick={() => GetRandomContact()}
+        onClick={() => GetRandomContact(props)}
       >
         Add Random Contact
       </button>
