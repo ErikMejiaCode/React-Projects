f7b92f6994c63bb6f2683874b0eb7381ae885250
Erik Mejia <erikdojo352@gmail.com>
2023-04-12 21:17:15 -0400

    Random api configured

---
+++ cyclopedia/src/Components/CyclOPediaClassPage.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { getRandomUser } from "../Utilities/api";
+
+class CyclOPediaClassPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instructor: undefined,
+      studentList: [],
+      studentCount: 0,
+      hideInstructor: false,
+    };
+  }
+
+  componentDidMount = async () => {
+    console.log("Component did Mount");
+    const response = await getRandomUser();
+    console.log(response);
+    this.setState((prevState) => {
+      return {
+        instructor: {
+          name: response.data.first_name + " " + response.data.last_name,
+          email: response.data.email,
+          phone: response.data.phone_number,
+        },
+      };
+    });
+  };
+
+  componentDidUpdate() {
+    console.log("Component did Update");
+  }
+
+  componentWillUnmount() {
+    console.log("Component will Mount");
+  }
+
+  render() {
+    console.log("Render Component");
+    return (
+      <div>
+        {this.state.instructor && (
+          <div className="p-3">
+            <span className="h4 text-success">Instructor</span>
+            <i className="bi bi-toggle-off btn btn-success btn-sm"></i>
+            <br />
+            Name : {this.state.instructor.name}
+            <br />
+            Email : {this.state.instructor.email}
+            <br />
+            Phone : {this.state.instructor.phone}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default CyclOPediaClassPage;
