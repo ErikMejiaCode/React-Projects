725562675b54afda5b9b2e3095b98a11fbe6de02
Erik Mejia <erikdojo352@gmail.com>
2023-04-13 19:18:35 -0400

    functional components and props

--- watchopedia/src/Counter.jsx
+++ watchopedia/src/Counter.jsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
 
 const Counter = () => {
-  const [counter, setCounter] = useState(() => {
-    console.log("Hello");
-    return 10;
+  const [counterState, setCounterState] = useState(() => {
+    return {
+      counter: 10,
+      title: "Fun",
+    };
   });
 
+  const [titleState, setTitleState] = useState("Fun");
+
   function incrementCounter() {
-    setCounter(counter + 1);
+    setCounterState((prevState) => {
+      return { ...counterState, counter: prevState.counter + 1 };
+    });
   }
   function decrementCounter() {
-    setCounter((prevState) => {
-      return prevState - 1;
+    setCounterState((prevState) => {
+      return { ...counterState, counter: prevState.counter - 1 };
     });
   }
 
   return (
     <div className="col-12 col-md-4 offset-md-4 border text-white">
-      <span className="h2 pt-4 m-2 text-white-50">Fun Counter</span>
+      <span className="h2 pt-4 m-2 text-white-50">{titleState} Counter</span>
       <br />
       <button onClick={incrementCounter} className="btn btn-success m-1">
         +1
@@ -28,7 +34,7 @@ const Counter = () => {
       <br />
       <span className="h4">
         Counter: &nbsp;
-        <span className="text-success">{counter}</span>
+        <span className="text-success">{counterState.counter}</span>
       </span>
     </div>
   );
