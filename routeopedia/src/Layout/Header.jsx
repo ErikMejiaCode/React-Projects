c480b92f2f7330734c323903ba5a91942257fe0c
Erik Mejia <erikdojo352@gmail.com>
2023-04-18 17:31:33 -0400

    bootstrap navbar added. additional components

--- routeopedia/src/Layout/Header.jsx
+++ routeopedia/src/Layout/Header.jsx
@@ -1,15 +1,79 @@
+import React from "react";
 import logo from "../images/react.png";
 
-const Header = () => {
+function Header() {
   return (
-    <div
-      className="py-2 pl-2"
-      style={{ borderBottom: "1px solid #777", backgroundColor: "black" }}
-    >
-      <img src={logo} alt="" style={{ height: "35px", verticalAlign: "top" }} />
-      <span className="h2 pt-4 m-2 text-white-50">RouteOPedia</span>
+    <div>
+      <nav
+        className="navbar navbar-expand-lg bg-body-tertiary"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <img
+            src={logo}
+            alt=""
+            style={{ height: "35px", verticalAlign: "top" }}
+          />{" "}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
-};
+}
 
 export default Header;
