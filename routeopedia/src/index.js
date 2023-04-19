9d18b9ee0c605776f489ac734b625f5297b14b19
Erik Mejia <erikdojo352@gmail.com>
2023-04-19 11:27:34 -0400

    React Router Dom, basic navigation

--- routeopedia/src/index.js
+++ routeopedia/src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Layout/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <Header />
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   </div>
 );
