a07a27a2e52f94dc4b8e078f7b81f5a3fdea2088
Erik Mejia <erikdojo352@gmail.com>
2023-04-19 15:48:56 -0400

    Navigation- useNavigate, Navigate, Link & useState

--- routeopedia/src/Pages/Product.jsx
+++ routeopedia/src/Pages/Product.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, Link, Navigate } from "react-router-dom";
 
 function Product() {
-  return <div>Product</div>;
+  const navigate = useNavigate();
+  const [goToProduct, setGoToProduct] = useState(() => {
+    return false;
+  });
+
+  return (
+    <div>
+      // Routing using useNavigate Product
+      <br />
+      <button
+        onClick={() => {
+          navigate("/product/create");
+        }}
+      >
+        Add Product
+      </button>
+      // Routing using Link
+      <Link to={`/product/details/5`}>
+        <button>Navigate to Product Detail - 5</button>
+      </Link>
+      // Routing using Navigate & UseState
+      {goToProduct && <Navigate to="/product/details/3" />}
+      <button
+        onClick={() => {
+          setGoToProduct({ goToProduct: true });
+        }}
+      >
+        Navigate to Product - 3 (useState)
+      </button>
+    </div>
+  );
 }
 
 export default Product;
