2591a994f5e5759a4b32a3e165b30591675b643b
Erik Mejia <erikdojo352@gmail.com>
2023-04-24 17:22:44 -0700

    post, put and delete destinations

--- travelopedia/src/api/destinationApi.jsx
+++ travelopedia/src/api/destinationApi.jsx
@@ -5,11 +5,37 @@ export const destinationApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
   endpoints: (builder) => ({
     //QUERY -> GET
-    //MUTATION -> POST/PUT/DELETE
     getAllDestination: builder.query({
       query: () => "destination",
+    }),
+    //MUTATION -> POST/PUT/DELETE
+    addDestination: builder.mutation({
+      query: (destination) => ({
+        url: "destination",
+        method: "POST",
+        body: destination,
+      }),
+    }),
+    updateDestination: builder.mutation({
+      query: (destination) => ({
+        url: `destination/${destination.id}`,
+        method: "PUT",
+        body: destination,
+      }),
+    }),
+    deleteDestination: builder.mutation({
+      query: ({ id }) => ({
+        url: `destination/${id}`,
+        method: "DELETE",
+        body: id,
+      }),
     }),
   }),
 });
 
-export const { useGetAllDestinationQuery } = destinationApi;
+export const {
+  useGetAllDestinationQuery,
+  useAddDestinationMutation,
+  useUpdateDestinationMutation,
+  useDeleteDestinationMutation,
+} = destinationApi;
