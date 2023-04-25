85ee97b90d06d10c2a60e044680a086a263086b6
Erik Mejia <erikdojo352@gmail.com>
2023-04-25 12:17:42 -0700

    integrate RTK with Redux Store

--- travelopedia/src/index.js
+++ travelopedia/src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Layout/Header";
 import DestinationIndex from "./Components/DestinationIndex";
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+// import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import { destinationApi } from "./api/destinationApi";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <ApiProvider api={destinationApi}>
+    <Provider store={store}>
       <Header />
       <DestinationIndex />
-    </ApiProvider>
+    </Provider>
   </div>
 );
