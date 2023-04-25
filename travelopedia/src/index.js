11117d27ea08af2984cb4e4e3309ddab8e024b46
Erik Mejia <erikdojo352@gmail.com>
2023-04-24 17:10:23 -0700

    fetching records from api

--- travelopedia/src/index.js
+++ travelopedia/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import Header from "./Layout/Header";
 import DestinationIndex from "./Components/DestinationIndex";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { destinationApi } from "./api/destinationApi";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <Header />
-    <DestinationIndex />
+    <ApiProvider api={destinationApi}>
+      <Header />
+      <DestinationIndex />
+    </ApiProvider>
   </div>
 );
