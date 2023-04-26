00784e60e685512368dd632fd1d505f93eae99e5
Erik Mejia <erikdojo352@gmail.com>
2023-04-25 18:18:34 -0700

    Project cards added

--- projectopedia/src/Pages/Home.jsx
+++ projectopedia/src/Pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import projectlist from "../Store/data";
+import ProjectCard from "../Components/Page/ProjectCard";
 
 function Home() {
   return (
     <div className="container p-5 text-white">
       <div className="h3 text-info">Project List</div>
       <div className="row">
-        {projectlist.map((project, index) => {
-          return <div key={index}>{project.title}</div>;
-        })}
+        {projectlist.map((project, index) => (
+          <ProjectCard key={index} />
+        ))}
       </div>
     </div>
   );
