5772cb226c17c2db0e87e23ae5cb0e31f6171663
Erik Mejia <erikdojo352@gmail.com>
2023-04-25 18:52:17 -0700

    Project card redirect link function

--- projectopedia/src/Pages/Home.jsx
+++ projectopedia/src/Pages/Home.jsx
@@ -4,11 +4,11 @@ import ProjectCard from "../Components/Page/ProjectCard";
 
 function Home() {
   return (
-    <div className="container p-5 text-white">
+    <div className="container p-5 text-black">
       <div className="h3 text-info">Project List</div>
       <div className="row">
         {projectlist.map((project, index) => (
-          <ProjectCard key={index} />
+          <ProjectCard key={index} project={project} />
         ))}
       </div>
     </div>
