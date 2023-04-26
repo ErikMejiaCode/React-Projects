de3affdf4f023720df1a5bedc7b033f1aacf2829
Erik Mejia <erikdojo352@gmail.com>
2023-04-25 21:15:18 -0700

    topics covered page added

--- projectopedia/src/Components/Page/TopicsCovered.jsx
+++ projectopedia/src/Components/Page/TopicsCovered.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import projectlist from "../../Store/data";
 
 function TopicsCovered() {
-  return <div className="text-white">TopicsCovered</div>;
+  return (
+    <div className="container p-5 text-white">
+      <div className="h3 text-info">Topics Covered</div>
+      <hr />
+      <div className="row">
+        {projectlist.map((project, index) => (
+          <div key={index} className="border rounded m-3 p-2">
+            <div className="h5 text-info">{project.title}</div>
+            <p>{project.description}</p>
+            <ul>
+              {project.topicsCovered.map((topic, index) => (
+                <li key={index}>{topic}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default TopicsCovered;
